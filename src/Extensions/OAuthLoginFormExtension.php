d34fb5b0f989d4f70b0793fc26002f31af638007
James (DPC) <james.ellis@dpc.nsw.gov.au>
2023-03-14 11:41:00 +1100

    Add check on array type

--- src/Extensions/OAuthLoginFormExtension.php
+++ src/Extensions/OAuthLoginFormExtension.php
@@ -19,6 +19,9 @@ class OAuthLoginFormExtension extends Extension
      */
     public function updateFormActions(&$actions) {
         $providers = Config::inst()->get($this->owner->getAuthenticatorClass(), 'providers');
+        if(!is_array($providers)) {
+            return;
+        }
         foreach ($providers as $provider => $config) {
             $name = isset($config['name']) ? $config['name'] : $provider;
             $action = $actions->fieldByName('action_authenticate_'. $provider);
