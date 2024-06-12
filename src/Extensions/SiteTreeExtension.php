7e4e5b282eec2011079e6607e291c7c0f99c983b
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2024-06-12 12:30:34 +1000

    Replace metatags with core meta handling. Add an extension to conditionally add preview meta. Disallow generator meta

---
+++ src/Extensions/SiteTreeExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace NSWDPC\Waratah\Extensions;
+
+use Silverstripe\Control\Controller;
+use Silverstripe\ORM\DataExtension;
+use SilverStripe\Security\Permission;
+use SilverStripe\Versioned\Versioned;
+
+class SiteTreeExtension extends DataExtension
+{
+    /**
+     * Update meta components as required
+     * @param array $tags
+     */
+    public function MetaComponents(array &$tags)
+    {
+
+        // conditionally add these components based on CMS preview mode being in place
+        unset($tags['pageId']);
+        unset($tags['cmsEditLink']);
+        $controller = Controller::curr();
+        $request = $controller->getRequest();
+        if (Permission::check('CMS_ACCESS_CMSMain')
+            && $this->owner->ID > 0
+            && Versioned::get_stage() === Versioned::DRAFT
+            && $request->getVar('CMSPreview') == 1
+        ) {
+            $tags['pageId'] = [
+                'attributes' => [
+                    'name' => 'x-page-id',
+                    'content' => $this->owner->ID,
+                ],
+            ];
+            $tags['cmsEditLink'] = [
+                'attributes' => [
+                    'name' => 'x-cms-edit-link',
+                    'content' => $this->owner->CMSEditLink(),
+                ],
+            ];
+        }
+
+    }
+}
