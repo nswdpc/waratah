a36bc675d79b12f2b1b15184f095d0170da5c93b
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-10-28 15:08:13 +1100

    Add style formats, importcss_append

--- _config.php
+++ _config.php
@@ -1,4 +1,6 @@
 <?php
+
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use SilverStripe\Forms\HTMLEditor\HtmlEditorConfig;
 
 HtmlEditorConfig::get('cms')->setOption('table_class_list', [
@@ -17,5 +19,143 @@ HtmlEditorConfig::get('cms')->setOption('table_class_list', [
     [
         'title' => 'Stripe and bordered',
         'value' => 'nsw-table nsw-table--striped nsw-table--bordered'
+    ]
+]);
+
+
+TinyMCEConfig::get('cms')->setOptions([
+    'importcss_append' => true,
+    'style_formats' => [
+        [
+            'title' => 'Blocks',
+            'items' => [
+                [
+                    'title' => 'Paragraph',
+                    'block' => 'p',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Paragraph (Intro)',
+                    'selector' => 'p',
+                    'attributes' => [
+                        'class' => 'nsw-intro'
+                    ]
+                ],
+            ]
+        ],
+        [
+            'title' => 'Headings',
+            'items' => [
+                [
+                    'title' => 'Heading 1',
+                    'block' => 'h1',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Heading 2',
+                    'block' => 'h2',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Heading 3',
+                    'block' => 'h3',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Heading 4',
+                    'block' => 'h4',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Heading 5',
+                    'block' => 'h5',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ],
+                [
+                    'title' => 'Heading 6',
+                    'block' => 'h6',
+                    'attributes' => [
+                        'class' => ''
+                    ]
+                ]
+            ]
+        ],
+        [
+            'title' => 'Buttons',
+            'items' => [
+                [
+                    'title' => 'Dark',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--dark'
+                    ]
+                ],
+                /*
+                [
+                    'title' => 'Dark Outline',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--dark-outline'
+                    ]
+                ],
+                [
+                    'title' => 'Dark Outline Solid',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--dark-outline-solid'
+                    ]
+                ],
+                */
+                [
+                    'title' => 'Light',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--light'
+                    ]
+                ],
+                /*
+                [
+                    'title' => 'Light Outline',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--light-outline'
+                    ]
+                ],
+                [
+                    'title' => 'White',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--white'
+                    ]
+                ],
+                [
+                    'title' => 'White Outline',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--white-outline'
+                    ]
+                ],
+                */
+                [
+                    'title' => 'Danger',
+                    'selector' => 'a',
+                    'attributes' => [
+                        'class' => 'nsw-button nsw-button--danger'
+                    ]
+                ]
+            ],
+        ]
     ]
 ]);
